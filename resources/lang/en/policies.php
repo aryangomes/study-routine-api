9390968f7822a7a390c19570e5430499897cd8b9
Aryan Gomes <aryan.gomes@gmail.com>
2022-03-12 11:39:22 -0300

    feat: :globe_with_meridians: Create policies lang resourcers

---
+++ resources/lang/en/policies.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'user_cannot_view' => 'User it is not allowed to view this :record',
+    'user_cannot_get_all' => 'User it is not allowed to get all this :record',
+    'user_cannot_create' => 'User it is not allowed to create a :record',
+    'user_cannot_update' => 'User it is not allowed to update this :record',
+    'user_cannot_delete' => 'User it is not allowed to delete this :record',
+];
