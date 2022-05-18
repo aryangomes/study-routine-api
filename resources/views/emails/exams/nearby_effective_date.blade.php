6c64787de7b9f16e38c53e8aaa43bcb07008ac8f
Aryan Gomes <aryan.gomes@gmail.com>
2022-05-18 11:14:35 -0300

    feat: Change effective date format is showed

--- resources/views/emails/exams/nearby_effective_date.blade.php
+++ resources/views/emails/exams/nearby_effective_date.blade.php
@@ -1,7 +1,8 @@
 @component('mail::message')
 # Hi, {{$user->name}}
 
-We are here to remember you about your Exam of Subject **{{$subject->name}}**, that will be in *{{$exam->effective_date->format('Y-m-d')}}*.
+We are here to remember you about your Exam of Subject **{{$subject->name}}**, that will be in
+*{{$exam->effective_date->format('l, M d, Y')}}*.
 
 **Have a good studies and a good day!**<br>
 **{{ config('app.name') }}**
