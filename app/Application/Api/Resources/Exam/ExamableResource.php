ff5f0d1fce74ecc1492c98c367ff0cfbca660eeb
Aryan Gomes <aryan.gomes@gmail.com>
2022-06-11 11:05:40 -0300

    feat: Create Examable Resource
    Created Examable Resource. Contains exams attributes and a examable attribute

---
+++ app/Application/Api/Resources/Exam/ExamableResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Application\Api\Resources\Exam;
+
+use App\Application\Api\Resources\Subject\SubjectResource;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ExamableResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'effective_date' => $this->effective_date,
+            'subject' => new SubjectResource($this->subject),
+            'examable' => $this->examable,
+        ];
+    }
+}
